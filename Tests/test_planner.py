8b9bddf4269f6fe555d7a1b166fc3988f62c5b73
kmwangemi <mwangemik@gmail.com>
2018-05-30 11:32:36 +0300

    my initial tests

---
+++ Tests/test_planner.py
@@ -0,0 +1,38 @@
+
+import unittest
+
+class TestPlanner(unittest.TestCase):
+
+    def setUp(self):
+        self.plan = Planner()
+
+        self.user1 = self.plan.create_user('Bruno', 'Herbert', 'bruno',
+                              '1234', 'silverjimy2@gmail.com')
+
+
+        # self.user('Bruno', 'Herbert', 'bruno','1234', 'silverjimy2@gmail.com')
+        self.user2 = self.plan.create_user('Okello', 'jackson', 'okello', 'admin', 'okeloo_admin@google.com')
+    def test_create_user_success(self):
+        """Test a user is created successfully"""
+        # self.plan.create_user('Judy', 'Herbert', 'bruno', '1234', 'silverjimmy2@gmail.com')
+        self.assertEqual(len(self.plan.users), 2,
+                         msg="Length of the the users dictionary should be 4")
+    def test_create_user_fail(self):
+        """Test failure due to duplicate username"""
+        # user1 = self.plan.create_user(self.user)
+        user2 = self.plan.create_user('Bruno',
+                                      'Herbert',
+                                      'bruno',
+                                      '1234',
+                                      'silverjimmy2@gmail.com')
+        self.assertEqual([self.user1, user2], ['Success', 'Fail'])
+
+    def test_login_user_success(self):
+        """Method updates the loged_in variable of the Planner class"""
+        online_before = len(Planner.loged_in)
+        self.plan.login_user('okello', 'admin')
+        online_after = len(Planner.loged_in)
+        self.assertEqual(online_after, online_before + 1)
+
+if __name__ == '__main__':
+    unittest.main()
